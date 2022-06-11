7f8e99efed972eaea992dbd5b001fe9f36fdc679
Valerian Pereira <valerianpereira25@gmail.com>
2022-06-11 11:41:04 +0530

    Create random_music.py

---
+++ random_music.py
@@ -0,0 +1,11 @@
+import random, os
+
+music_dir = '/Users/valerianpereira/Desktop'
+
+songs = os.listdir(music_dir)
+song = random.randint(0,len(songs))
+
+## Prints The Song Name
+print(songs[song])
+
+os.startfile(os.path.join(music_dir, songs[0])) 
